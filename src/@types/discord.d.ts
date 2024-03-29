15bf61d779d912c0a59ee5e7b3977339c5471f93
caleb <calebsg2253284@gmail.com>
2024-03-29 14:37:48 -0400

    add cooldown functionality

--- src/@types/discord.d.ts
+++ src/@types/discord.d.ts
@@ -7,7 +7,7 @@ import SudokuHandler from "../model/sudoku/sudokuHandler";
 declare module "discord.js" {
   export interface Client {
     commands: Collection<string, SlashCommand>,
-    cooldowns: Collection<string, Collection<string, Date>>,
+    cooldowns: Collection<string, Collection<string, number>>,
     sudokuSessions: Collection<string, SudokuHandler>
   }
 }
--- src/listeners/discord/interactionCreate.ts
+++ src/listeners/discord/interactionCreate.ts
@@ -1,6 +1,6 @@
 // handle user bot interactions
 
-import { ChatInputCommandInteraction, Events, Interaction } from "discord.js";
+import { ChatInputCommandInteraction, Events, Interaction, Collection } from "discord.js";
 import chalk from "chalk";
 import { Listener } from "../_interface/Listener";
 
@@ -25,12 +25,43 @@ const handleSlashCommands = async (interaction: ChatInputCommandInteraction) => 
     });
   }
 
+  const { cooldowns } = interaction.client;
+  const cmdName = command.data.name;
+  const user = interaction.user;
+
+  // creates collection of user cooldowns for specific command if it does not exist
+  if (!cooldowns.has(cmdName)) {
+    cooldowns.set(cmdName, new Collection());
+  }
+
+  const now = Date.now();
+
+  // collection of users with an active cooldown
+  const timestamps = cooldowns.get(cmdName);
+  const defaultCooldownSeconds = 1;
+
+  // set cooldown amount
+  const cooldownAmount = (command.cooldown ?? defaultCooldownSeconds) * 1000;
+
+  if (timestamps.has(user.id)) {
+    const expirationTime = timestamps.get(user.id) + cooldownAmount;
+    return interaction.reply({
+      content: `You're too fast, I can't keep up! Please wait <t:${Math.round(expirationTime / 1000)}:R>`,
+      ephemeral: true
+    }).then(msg => {
+      setTimeout(() => msg.delete(), expirationTime - now);
+    });
+  }
+
+  timestamps.set(user.id, now);
+  setTimeout(() => timestamps.delete(user.id), cooldownAmount);
+
   try {
     command.execute(interaction);
   } catch (err) {
     console.error(chalk.red(err));
     const reply = {
-      content: `There was an issue running the command \`${command.data.name}\`.`,
+      content: `There was an issue running the command \`${cmdName}\`.`,
       ephemeral: true
     }
     if (interaction.deferred || interaction.replied) {
